09bdccb742f31660f6b4510dbe2b545a770ec00b
AlexisBelloc <alexis.belloc@outlook.com>
2018-12-28 11:44:11 +0100

    news contents to db

--- app/src/main/java/com/example/neiris/embeddedplatform/DatabaseHelper.java
+++ app/src/main/java/com/example/neiris/embeddedplatform/DatabaseHelper.java
@@ -17,9 +17,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String COLUMN_EMAIL = "email";
     private static final String COLUMN_UNAME = "uname";
     private static final String COLUMN_PASS = "pass";
+    private static final String COLUMN_CONTENT = "content";
     SQLiteDatabase db;
     private static final String TABLE_CREATE="create table contacts (id integer primary key not null ," +
             "name text not null, email text not null, uname text not null, pass text not null);";
+    private static final String TABLE_CREATE2="create table articles (id integer primary key not null, name text not null, content text not null);";
 
     public DatabaseHelper(Context context)
     {
@@ -29,12 +31,65 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(TABLE_CREATE);
+        db.execSQL(TABLE_CREATE2); // inserting news table
+        System.out.println("DB findEditTable: " + db.findEditTable("articles"));
+//        db.execSQL("create table articles (id integer primary key not null, name text not null, content text not null);");
         this.db = db;
+    }
+
+    public void fillArticlesInDb() {
+        db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        // parse files & assign ids
+        int articleId = 0;
+        String articleName = "Home Workout";
+        // article content, might get it from web
+        String articleContent = "180 Squat Jumps\n" +
+                "\n" +
+                "A. Stack the toilet paper rolls on top of each other. Stand facing the stack with feet wider than hip-width apart.\n" +
+                "B. Lower into a squat to grab the first roll, exploding up into a jump and turning 180 degrees to face the other way.\n" +
+                "C. Upon landing, immediately lower into another squat placing the toilet paper on the ground.\n" +
+                "D. Repeat, unstacking the toilet paper on one side, and creating a new stack on the other side.\n" +
+                "\n" +
+                "Do AMRAP for 20 seconds; rest for 10 seconds.\n" +
+                "Military Plank TP Stack\n" +
+                "\n" +
+                "A. Start in a high plank position with toilet paper in one stack in front of the right shoulder.\n" +
+                "B. Shift right palm over so it's on the floor beneath the center of the chest.\n" +
+                "C. Maintaining a strong, straight plank position, grab the top roll of toilet paper and place it on the floor about a foot to the left. Repeat, moving each roll to create a new stack.\n" +
+                "\n" +
+                "Do AMRAP for 20 seconds; rest for 10 seconds. Do every other set on the opposite side.\n" +
+                "Single-Leg Around-the-World Floor Tap\n" +
+                "\n" +
+                "A. Place toilet paper rolls on floor in a semicircle. Stand on the left leg facing the rolls.\n" +
+                "B. Keeping right leg lifted, squat to tap the left-most roll with the right hand. Stand, then squat to touch the next roll, repeating the movement while moving down the line.\n" +
+                "C. After tapping the right-most roll, reverse the direction.\n" +
+                "\n" +
+                "Do AMRAP for 20 seconds; rest for 10 seconds. Do every other set on the opposite side.";
+
+        values.put(COLUMN_ID, articleId);
+        values.put(COLUMN_NAME, articleName);
+        values.put(COLUMN_CONTENT, articleContent);
+        System.out.println("[INSERTING IN DB]");
+
+        db.insert("articles", null, values);
+        db.close();
+    }
+
+    public String getArticle(int id) {
+        db = this.getReadableDatabase();
+        String query = "select content from articles where id=" + id;
+
+        // getting specific article from id
+        Cursor cursor = db.rawQuery(query, null);
+        cursor.moveToFirst();
+        return cursor.getString(0);
     }
 
     public void insertContact(Contact c)
     {
-        db = this.getWritableDatabase();
+        db = this.getWritableDatabase(); // pourquoi pas la créer dans le onCreate?
         ContentValues values = new ContentValues();
 
         //create a string query
@@ -56,7 +111,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public String searchPass(String name){
 
         db = this.getReadableDatabase();
-        String query = "select name, pass from " +TABLE_NAME;
+        String query = "select name, pass from " + TABLE_NAME;
         Cursor cursor = db.rawQuery(query, null);
         String a, b;
         b="not found";
@@ -76,7 +131,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        String query = "DROP TABLE IF EXISTS"+TABLE_NAME;
+        String query = "DROP TABLE IF EXISTS" + TABLE_NAME;
         db.execSQL(query);
         this.onCreate(db);
     }
--- app/src/main/java/com/example/neiris/embeddedplatform/News.java
+++ app/src/main/java/com/example/neiris/embeddedplatform/News.java
@@ -1,17 +1,27 @@
 package com.example.neiris.embeddedplatform;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class News extends AppCompatActivity {
+    DatabaseHelper helper = new DatabaseHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+        helper.fillArticlesInDb();
+        System.out.println("---[NEWS PAGE CREATED]---");
     }
 
+    public void displayArticle(View V) {
+        TextView editTxtArticle = (TextView) findViewById(R.id.textViewNews);
+        // prevents crash
+//        String articleTxt = helper.getArticle(0);
+//        editTxtArticle.setText(articleTxt);
+
+    }
 
 }
